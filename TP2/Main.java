3c06e93f3f10e421010c609b75607487505d0a6b
ilyass ikrimah <123764243+ilyasstop500@users.noreply.github.com>
2023-10-01 17:05:45 +0200

    Create Main.java

---
+++ TP2/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Population P1 = new Population("moghrib");
+
+        P1.ShowPopulation();
+
+        P1.Croisement().Croisement().Croisement().ShowPopulation();
+
+    }
+}
