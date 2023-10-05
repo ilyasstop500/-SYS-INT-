5f7d06f193822194d3540efb256912548f217262
ilyass ikrimah <123764243+ilyasstop500@users.noreply.github.com>
2023-10-05 08:23:26 +0000

    j

--- TP2/Main.java
+++ TP2/Main.java
@@ -4,7 +4,7 @@ public class Main {
         Population P1 = new Population("moghrib");
 
         P1.ShowPopulation();
-        P1.Trouver_max("Mutation", 2000000000).ShowSolution() ;
+        P1.Trouver_max("Mutation", 20000).ShowSolution() ;
 
        
     }   
